2879a34b6a0d4e2c239395d4b209a49b144918f9
Vdiicntioerl <victor.diniel@etu.univ-orleans.fr>
2023-03-26 16:39:24 +0200

    Fernet

--- FernetGUI.py
+++ FernetGUI.py
@@ -1,34 +1,62 @@
 import os
 import logging
 from ciphered_gui import CipheredGUI
+from chat_client import ChatClient
 from cryptography.fernet import Fernet
-from hashlib
+import dearpygui.dearpygui as dpg
+import hashlib
+import base64
+from generic_callback import GenericCallback
 
+DEFAULT_VALUES = {
+    "host" : "127.0.0.1",
+    "port" : "6666",
+    "name" : "foo",
+    "password" : ""
+}
 class FernetGUI(CipheredGUI):
     def __init(self) -> None:
         super().__init__()
         self._key =None
+
     def key_derivation(self, password: str) -> bytes:
-        #Salt genere aleatoirement
-        salt="Bonneoumauvaisesituation".encode()
-        #conversion du mot de passe en bytes
-        password_bytes = password.encode()
-        #password_bytes=password.encode('utf-8')
-        #derivation
-        kdf = Fernet(algorithm=hashes.SHA256(),
-                        length=32,
-                        salt=salt,
-                        iterations=480000)
-        key = kdf.derive(password_bytes)
+        #Hashage du mot de passe puis recuperation de l'empreinte
+        key=hashlib.sha256(password.encode()).digest()
+        key = base64.b64encode(key)
         return key
     
 
     def run_chat(self, sender, app_data) -> None:
-        return super().run_chat(sender, app_data)
+        # callback used by the connection windows to start a chat session
+        host = dpg.get_value("connection_host")
+        port = int(dpg.get_value("connection_port"))
+        name = dpg.get_value("connection_name")
+        password = dpg.get_value("connection_password")
+        self._log.info(f"Connecting {name}@{host}:{port}")
+
+        self._callback = GenericCallback()
+
+        self._client = ChatClient(host, port)
+        self._client.start(self._callback)
+        self._client.register(name)
+
+        dpg.hide_item("connection_windows")
+        dpg.show_item("chat_windows")
+        dpg.set_value("screen", "Connecting")
+
+        #appel de la fonction de derivation de cle
+        self._key = self.key_derivation(password)
+    
     def encrypt(self, message):
-        return super().encrypt(message)
+        f=Fernet(self._key)
+        encrypted_message=f.encrypt(bytes(message,'utf-8'))
+        return encrypted_message
+    
     def decrypt(self, encrypted_message):
-        return super().decrypt(encrypted_message)
+        message = base64.b64decode(encrypted_message['data'])
+        f = Fernet(self._key)
+        message = f.decrypt(message).decode()
+        return message
     
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
